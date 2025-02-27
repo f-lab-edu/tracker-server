4159e5a5a753f0fea46454be7d1ca6d5801c2c75
임재원 <nemo0824@naver.com>
2025-02-27 21:29:35 +0900

    refactor: Routes 이름 변경.

--- src/routes/userDeviceRoutes.ts
+++ src/routes/userDeviceRoutes.ts
@@ -3,8 +3,11 @@ import { userDeviceController } from '../controllers/userDeviceController';
 
 export const userDeviceRouter = express.Router();
 
-userDeviceRouter.post('/userDevice', userDeviceController.saveUserDevice);
-userDeviceRouter.get('/userDevice/browser/:domain', userDeviceController.getBrowserStats);
-userDeviceRouter.get('/userDevice/os/:domain', userDeviceController.getOsStats);
-userDeviceRouter.get('/userDevice/device/:domain', userDeviceController.getDeviceStats);
-userDeviceRouter.get('/userDevice/resolution/:domain', userDeviceController.getResolutionStats);
+userDeviceRouter.post('/userDevices', userDeviceController.saveUserDevice);
+userDeviceRouter.get('/userDevice/browsers-stats/:domain', userDeviceController.getBrowserStats);
+userDeviceRouter.get('/userDevice/os-stats/:domain', userDeviceController.getOsStats);
+userDeviceRouter.get('/userDevice/device-stats/:domain', userDeviceController.getDeviceStats);
+userDeviceRouter.get(
+  '/userDevice/resolution-stats/:domain',
+  userDeviceController.getResolutionStats
+);
