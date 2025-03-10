8af0afa37f79ddd1f85c4890fc1a7e024651ebfd
임재원 <nemo0824@naver.com>
2025-03-10 18:25:17 +0900

    refactor: dashboardClientService(apikey를 bcrypt 에서 sha256으로 변경)

--- src/services/dashboardClientService.ts
+++ src/services/dashboardClientService.ts
@@ -1,12 +1,11 @@
 import bcrypt from 'bcryptjs';
-import { v4 as UUIDV4 } from 'uuid';
+import * as crypto from 'crypto';
 import { DashboardClientModel } from '../models/dashboardClientModel';
 import { ClientType } from '../types/dashboardClientType';
-
 export const dashboardClientService = {
   enrollClient: async (email: string, password: string, domain: string) => {
-    const apiKey = UUIDV4();
-    const hashedApiKey = await bcrypt.hash(apiKey, 10);
+    const apiKey = crypto.randomBytes(32).toString('hex');
+    const hashedApiKey = crypto.createHash('sha256').update(apiKey).digest('hex');
     const hashedPassword = await bcrypt.hash(password, 10);
     await DashboardClientModel.create({
       email,
