ee183491994424c2adaab20ddcb0504554eb40c3
임재원 <nemo0824@naver.com>
2025-03-10 16:33:44 +0900

    refactor: errorHandle  글로벌 에러핸들러 수정,

--- src/middleware/errorHandle.ts
+++ src/middleware/errorHandle.ts
@@ -5,5 +5,11 @@ export const errorHandle = (err: Error, req: Request, res: Response, next: NextF
   if (res.headersSent) {
     return next(err);
   }
+  if (
+    err.message === '등록된 유저가 아닙니다.' ||
+    err.message === '비밀번호가 올바르지 않습니다.'
+  ) {
+    res.status(401).json({ message: err.message });
+  }
   res.status(500).json({ message: '서버 내부 오류', err: err.message });
 };
