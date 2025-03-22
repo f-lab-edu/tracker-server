1dfbc899623ba0025c89984a5450c57772de773b
임재원 <nemo0824@naver.com>
2025-03-22 20:54:59 +0900

    build : 배포 https로 배포

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express from 'express';
+import './config/dbConnection';
 import { errorHandle } from './middleware/errorHandle';
 import { createSession } from './middleware/session';
 import { dashboardRouter } from './routes/dashboardRoutes';
@@ -25,6 +26,10 @@ app.use(createSession);
 app.use('/dashboard', dashboardRouter);
 app.use('/trackerSdk', trackerSdkRouter);
 app.use(errorHandle);
+
+app.get('/', (req, res) => {
+  res.send('tracker-sdk 서버 정상 작동 중!');
+});
 
 app.listen(port, () => {
   console.log(`🚀 서버 실행 중 👉 http://localhost:${port}`);
